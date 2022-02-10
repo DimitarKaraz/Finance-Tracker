ffd321d0d05cd50c082d90fceeba32c63960bbf9
Panayotov42 <rayleeanna9@gmail.com>
2022-02-10 22:29:52 +0200

    adding spring project

---
+++ FinanceTracker/src/main/java/com/example/financetracker/FinanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.financetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceTrackerApplication.class, args);
+    }
+
+}
---
+++ FinanceTracker/src/test/java/com/example/financetracker/FinanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.financetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
