4ef7192f8447c3df9c45cba5f9db76ec12d4725e
Dimitar Karazapryanov <dkarazapr@gmail.com>
2022-02-11 20:33:33 +0200

    useless commit

--- FinanceTracker/src/main/java/com/example/financetracker/controller/UserController.java
+++ FinanceTracker/src/main/java/com/example/financetracker/controller/UserController.java
@@ -1,22 +1,25 @@
 package com.example.financetracker.controller;
 
+import com.example.financetracker.model.User;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")    // ?
+@RequestMapping("/user")    // ?
 public class UserController {
 
     @Autowired
     private ModelMapper modelMapper;
 
 
-    @PostMapping
-    public User register() {
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+
 
     }
 
