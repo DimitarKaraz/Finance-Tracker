9b912f3a26acb562a3338a300b80d80c9fd3ff59
Panayotov42 <rayleeanna9@gmail.com>
2022-02-12 20:33:28 +0200

    fixed missing annotations

--- FinanceTracker/src/main/java/com/example/financetracker/model/dto/CreateAccountRequestDTO.java
+++ FinanceTracker/src/main/java/com/example/financetracker/model/dto/CreateAccountRequestDTO.java
@@ -11,4 +11,5 @@ import org.springframework.stereotype.Component;
 @Setter
 @NoArgsConstructor
 public class CreateAccountRequestDTO {
+
 }
--- FinanceTracker/src/main/java/com/example/financetracker/model/repositories/AccountTypeRepository.java
+++ FinanceTracker/src/main/java/com/example/financetracker/model/repositories/AccountTypeRepository.java
@@ -2,7 +2,9 @@ package com.example.financetracker.model.repositories;
 
 import com.example.financetracker.model.pojo.AccountType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccountTypeRepository extends JpaRepository<AccountType, Integer> {
     
 }
