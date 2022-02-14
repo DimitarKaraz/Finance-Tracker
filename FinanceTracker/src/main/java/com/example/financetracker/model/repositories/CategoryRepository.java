cd5ad048ef987153f3b80d68dc85cee645dd069a
Panayotov42 <rayleeanna9@gmail.com>
2022-02-14 23:06:03 +0200

    added additional verification to createCategory

--- FinanceTracker/src/main/java/com/example/financetracker/model/repositories/CategoryRepository.java
+++ FinanceTracker/src/main/java/com/example/financetracker/model/repositories/CategoryRepository.java
@@ -12,5 +12,8 @@ public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
     List<Category> findAllByUser_UserIdOrUser_UserIdIsNull(int userId);
 
-    //Category findByUser_UserIdAndNameOrUser_UserIdIsNullAndName();
+    //Category findByUser_UserIdAndNameOrUser_UserIdIsNullAndName(int userId, String name);
+
+    Category findByUser_UserIdAndName(int userId, String name);
+    Category findByNameAndUser_UserIdIsNull(String name);
 }
