9536d74fe711bda167ad16785cf819a0bfcf5888
Panayotov42 <rayleeanna9@gmail.com>
2022-02-16 17:47:38 +0200

    added CreateTransaction methods

--- FinanceTracker/src/main/java/com/example/financetracker/controller/TransactionController.java
+++ FinanceTracker/src/main/java/com/example/financetracker/controller/TransactionController.java
@@ -1,13 +1,28 @@
 package com.example.financetracker.controller;
 
+import com.example.financetracker.model.dto.ResponseWrapper;
+import com.example.financetracker.model.dto.transactionDTOs.TransactionCreateRequestDTO;
+import com.example.financetracker.model.dto.transactionDTOs.TransactionResponseDTO;
 import com.example.financetracker.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 public class TransactionController {
 
     @Autowired
     private TransactionService transactionService;
+
+    @PostMapping("/transactions/create")
+    private ResponseEntity<ResponseWrapper<TransactionResponseDTO>> createTransaction(@Valid @RequestBody TransactionCreateRequestDTO requestDTO){
+        return ResponseWrapper.wrap("Transaction created.", transactionService.createTransaction(requestDTO), HttpStatus.CREATED);
+    }
+
 
 }
--- FinanceTracker/src/main/java/com/example/financetracker/service/TransactionService.java
+++ FinanceTracker/src/main/java/com/example/financetracker/service/TransactionService.java
@@ -1,10 +1,50 @@
 package com.example.financetracker.service;
 
+import com.example.financetracker.model.dto.transactionDTOs.TransactionCreateRequestDTO;
+import com.example.financetracker.model.dto.transactionDTOs.TransactionResponseDTO;
+import com.example.financetracker.model.pojo.Budget;
+import com.example.financetracker.model.pojo.Category;
+import com.example.financetracker.model.pojo.Transaction;
+import com.example.financetracker.model.repositories.BudgetRepository;
+import com.example.financetracker.model.repositories.CategoryRepository;
+import com.example.financetracker.model.repositories.TransactionRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Service
 public class TransactionService {
 
+    @Autowired
+    private ModelMapper modelMapper;
+    @Autowired
+    private TransactionRepository transactionRepository;
+    @Autowired
+    private BudgetRepository budgetRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
-    //todo set date_time to now() when creating
+    @Transactional
+    public TransactionResponseDTO createTransaction(TransactionCreateRequestDTO requestDTO){
+        //todo validations
+        Transaction transaction = modelMapper.map(requestDTO, Transaction.class);
+        transaction.setDateTime(LocalDateTime.now());
+        Set<Budget> affectedBudgets = budgetRepository.findAllBudgetsByCategoryAndAccount(requestDTO.getAccountId(), requestDTO.getCategoryId());
+        for (Budget budget : affectedBudgets){
+            budget.setAmountSpent(budget.getAmountSpent().add(transaction.getAmount()));
+            //todo if budget is 75% spent or 100% spent send notifications/warnings to user
+            budgetRepository.save(budget);
+        }
+        transactionRepository.save(transaction);
+        return modelMapper.map(transaction, TransactionResponseDTO.class);
+    }
+
+
 }
