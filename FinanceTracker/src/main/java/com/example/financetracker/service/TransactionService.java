598981361335449144bdf890572adc731d3db472
Dimitar Karazapryanov <dkarazapr@gmail.com>
2022-02-16 18:19:42 +0200

    refined methods in TransactionService

--- FinanceTracker/src/main/java/com/example/financetracker/service/TransactionService.java
+++ FinanceTracker/src/main/java/com/example/financetracker/service/TransactionService.java
@@ -3,6 +3,7 @@ package com.example.financetracker.service;
 import com.example.financetracker.exceptions.BadRequestException;
 import com.example.financetracker.exceptions.NotFoundException;
 import com.example.financetracker.exceptions.UnauthorizedException;
+import com.example.financetracker.model.dto.transactionDTOs.TransactionCreateRequestDTO;
 import com.example.financetracker.model.dto.transactionDTOs.TransactionEditRequestDTO;
 import com.example.financetracker.model.dto.transactionDTOs.TransactionResponseDTO;
 import com.example.financetracker.model.pojo.Account;
@@ -10,15 +11,12 @@ import com.example.financetracker.model.pojo.Budget;
 import com.example.financetracker.model.pojo.Category;
 import com.example.financetracker.model.pojo.Transaction;
 import com.example.financetracker.model.repositories.*;
-import com.example.financetracker.model.dto.transactionDTOs.TransactionCreateRequestDTO;
-import com.example.financetracker.model.repositories.BudgetRepository;
-import com.example.financetracker.model.repositories.CategoryRepository;
-import com.example.financetracker.model.repositories.TransactionRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import javax.transaction.Transactional;
-import java.util.List;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Set;
 
@@ -45,10 +43,53 @@ public class TransactionService {
 
     @Transactional
     public TransactionResponseDTO editTransaction(TransactionEditRequestDTO requestDTO) {
+        BigDecimal subtractFromAffectedBudgets = validateEditRequest(requestDTO);
+
+        Transaction transaction = modelMapper.map(requestDTO, Transaction.class);
+        System.out.println("\n************** "  + transaction);
+        transactionRepository.save(transaction);
+
+        Set<Budget> affectedBudgets = budgetRepository.findAllBudgetsByCategoryAndAccount(requestDTO.getAccountId(), requestDTO.getCategoryId());
+        updateAffectedBudgets(subtractFromAffectedBudgets, transaction.getAmount(), affectedBudgets);
+
+        return convertToResponseDTO(transaction);
+    }
+
+    @Transactional
+    public TransactionResponseDTO createTransaction(TransactionCreateRequestDTO requestDTO){
+        //todo validations
+        Transaction transaction = modelMapper.map(requestDTO, Transaction.class);
+        transaction.setDateTime(LocalDateTime.now());
+        Set<Budget> affectedBudgets = budgetRepository.findAllBudgetsByCategoryAndAccount(requestDTO.getAccountId(), requestDTO.getCategoryId());
+
+        updateAffectedBudgets(new BigDecimal(0), transaction.getAmount(), affectedBudgets);
+
+        transactionRepository.save(transaction);
+        return convertToResponseDTO(transaction);
+    }
+
+    private void updateAffectedBudgets(BigDecimal oldTransactionAmount, BigDecimal newTransactionAmount, Set<Budget> affectedBudgets) {
+        for (Budget budget : affectedBudgets){
+            budget.setAmountSpent(budget.getAmountSpent().subtract(oldTransactionAmount));
+            budget.setAmountSpent(budget.getAmountSpent().add(newTransactionAmount));
+            //todo if budget is 75% spent or 100% spent send notifications/warnings to user
+            budgetRepository.save(budget);
+        }
+    }
+
+    private TransactionResponseDTO convertToResponseDTO(Transaction transaction) {
+        TransactionResponseDTO responseDTO = modelMapper.map(transaction, TransactionResponseDTO.class);
+        responseDTO.setCurrency(transaction.getAccount().getCurrency());
+        return responseDTO;
+    }
+
+    private BigDecimal validateEditRequest(TransactionEditRequestDTO requestDTO) {
         Transaction transaction = transactionRepository.findById((requestDTO.getTransactionId()))
                 .orElseThrow(() -> {
                     throw new NotFoundException("Invalid transaction id.");
                 });
+
+        BigDecimal subtractFromAffectedBudgets = transaction.getAmount();
 
         Account account = accountRepository.findById(requestDTO.getAccountId())
                 .orElseThrow(() -> {
@@ -76,31 +117,8 @@ public class TransactionService {
         if (!paymentMethodRepository.existsById(requestDTO.getPaymentMethodId())) {
             throw new NotFoundException("Invalid payment method id.");
         }
-        transaction = modelMapper.map(requestDTO, Transaction.class);
-        transactionRepository.save(transaction);
-
-//        Set<Budget> affectedBudgets = budgetRepository.findAllBudgetsByCategoryAndAccount(requestDTO.getAccountId(), requestDTO.getCategoryId());
-//        affectedBudgets.forEach(budget -> {
-//            budget.setBudgetId()
-//        });
+        return subtractFromAffectedBudgets;
     }
-
-    @Transactional
-    public TransactionResponseDTO createTransaction(TransactionCreateRequestDTO requestDTO){
-        //todo validations
-        Transaction transaction = modelMapper.map(requestDTO, Transaction.class);
-        transaction.setDateTime(LocalDateTime.now());
-        Set<Budget> affectedBudgets = budgetRepository.findAllBudgetsByCategoryAndAccount(requestDTO.getAccountId(), requestDTO.getCategoryId());
-        for (Budget budget : affectedBudgets){
-            budget.setAmountSpent(budget.getAmountSpent().add(transaction.getAmount()));
-            //todo if budget is 75% spent or 100% spent send notifications/warnings to user
-            budgetRepository.save(budget);
-        }
-        transactionRepository.save(transaction);
-        return modelMapper.map(transaction, TransactionResponseDTO.class);
-    }
-
-
 
 
 }
