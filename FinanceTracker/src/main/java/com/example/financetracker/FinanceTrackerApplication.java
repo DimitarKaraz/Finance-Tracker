8a93d65bdbb14bbaded4d32403c1fff0e4499ef7
Panayotov42 <rayleeanna9@gmail.com>
2022-02-18 20:58:56 +0200

    added CronJobs

--- FinanceTracker/src/main/java/com/example/financetracker/FinanceTrackerApplication.java
+++ FinanceTracker/src/main/java/com/example/financetracker/FinanceTrackerApplication.java
@@ -4,11 +4,13 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @SpringBootApplication
+@EnableScheduling
 public class FinanceTrackerApplication {
 
 
@@ -25,6 +27,7 @@ public class FinanceTrackerApplication {
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
+
 
 }
 
---
+++ FinanceTracker/src/main/java/com/example/financetracker/service/CronJobs.java
@@ -0,0 +1,6 @@
+package com.example.financetracker.service;
+
+
+public class CronJobs {
+        
+}
