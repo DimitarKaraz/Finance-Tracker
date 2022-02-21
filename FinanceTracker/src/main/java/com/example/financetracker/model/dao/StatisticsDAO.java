46d3fb03e41436df806f19231264cf188181f34d
Dimitar Karazapryanov <dkarazapr@gmail.com>
2022-02-21 16:48:56 +0200

    wrote getBudgetsByFilters SQL String

--- FinanceTracker/src/main/java/com/example/financetracker/model/dao/StatisticsDAO.java
+++ FinanceTracker/src/main/java/com/example/financetracker/model/dao/StatisticsDAO.java
@@ -1,12 +1,16 @@
 package com.example.financetracker.model.dao;
 
 import com.example.financetracker.model.dto.budgetDTOs.BudgetByFiltersDTO;
+import com.example.financetracker.model.dto.budgetDTOs.BudgetResponseDTO;
 import com.example.financetracker.model.pojo.Budget;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.math.BigDecimal;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -16,17 +20,54 @@ public class StatisticsDAO {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private ModelMapper modelMapper;
 
     public List<Budget> getBudgetsByFilters(BudgetByFiltersDTO filtersDTO) {
-        String sql = "SELECT * " +
-                "FROM budgets AS b " +
-                "";
+        String sql = "SELECT *\n" +
+                "FROM budgets AS b\n" +
+                "LEFT JOIN budgets_have_categories AS bhc ON (b.budget_id = bhc.budget_id)\n" +
+                "WHERE (b.start_date BETWEEN ? AND ?)\n";
 
+        if (filtersDTO.getAccountId() != null) {
+            sql += "AND (b.account_id = " +
+                    filtersDTO.getAccountId() +
+                    ")\n";
+        }
+        if (filtersDTO.getIntervalId() != null) {
+            sql += "AND (b.interval_id = " +
+                    filtersDTO.getIntervalId() +
+                    ")\n";
+        }
+        if (filtersDTO.getCategoryIds() != null) {
+            StringBuffer categoryIds = new StringBuffer();
+            filtersDTO.getCategoryIds()
+                    .forEach(integer -> categoryIds.append(integer).append(","));
+            categoryIds.deleteCharAt(categoryIds.length() - 1);
 
-        RowMapper<Budget> budgetRowMapper = new RowMapper<Budget>() {
+            sql += "AND (bhc.category_id IN (" +
+                    categoryIds +
+                    "))\n";
+        }
+        if (filtersDTO.getAmountMin() != null || filtersDTO.getAmountMax() != null) {
+            sql += "AND (b.max_limit BETWEEN " +
+                    (filtersDTO.getAmountMin() != null ? filtersDTO.getAmountMin() : "0.00") +
+                    " AND " +
+                    (filtersDTO.getAmountMax() != null ? filtersDTO.getAmountMax() : "9999999999999.99") +
+                    ");";
+        }
+
+        BudgetResponseDTO budgetRowMapper = new RowMapper<BudgetResponseDTO>() {
             @Override
-            public Budget mapRow(ResultSet rs, int rowNum) throws SQLException {
-                return null;
+            public BudgetResponseDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+                BudgetResponseDTO budgetResponseDTO = new BudgetResponseDTO();
+                budgetResponseDTO.setBudgetId(rs.getInt("budget_id"));
+                budgetResponseDTO.setName(rs.getString("name"));
+                budgetResponseDTO.setAmountSpent(rs.getBigDecimal("amount_spent"));
+                budgetResponseDTO.setMaxLimit(rs.getBigDecimal("max_limit"));
+                budgetResponseDTO.setInterval(modelMapper.map(r));
+
+
             }
         }
 
--- FinanceTracker/src/main/java/com/example/financetracker/model/repositories/TransactionRepository.java
+++ FinanceTracker/src/main/java/com/example/financetracker/model/repositories/TransactionRepository.java
@@ -6,22 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
 
-//    @Modifying
-//    @Query(value = "INSERT INTO transactions\n" +
-//                    "(transaction_type_id, amount, account_id, category_id, payment_method_id, date_time)\n" +
-//                    "VALUES\n" +
-//                    "(?, ?, ?, ?, ?, ?);", nativeQuery = true)
-//    void insertIntoDB(int transactionTypeId, BigDecimal amount, int accountId,
-//                             int categoryId, int paymentMethodId, LocalDateTime dateTime);
 
     List<Transaction> findAllByCategoryCategoryId(int categoryId);
 
@@ -33,8 +24,6 @@ public interface TransactionRepository extends JpaRepository<Transaction, Intege
 
     List<Transaction> findTransactionsByDateTimeAfter(LocalDateTime localDateTime);
 
-    List<Transaction> findTransactionsByAccountUserUserId(int userId);
-
     List<Transaction> findAllByDateTimeBetweenAndAccount_User_UserId(LocalDateTime start, LocalDateTime end, int userId);
 
     List<Transaction> findAllByAmountBetween(BigDecimal min, BigDecimal max);
@@ -44,8 +33,6 @@ public interface TransactionRepository extends JpaRepository<Transaction, Intege
     List<Transaction> findAllByTransactionType_TransactionTypeId(int transactionTypeId);
 
     List<Transaction> findAllByPaymentMethod_PaymentMethodId(int paymentMethodId);
-
-    //todo: (:myArray) OR ? -> String..of..(int[] myArray);
 
 
 }
