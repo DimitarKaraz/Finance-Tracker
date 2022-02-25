0428ff956d4cad8b1f297ebc38faf925c01c235c
Dimitar Karazapryanov <dkarazapr@gmail.com>
2022-02-26 01:42:14 +0200

    added main classes to ExchangeRateService project

---
+++ ExchangeRateService/src/main/java/com/example/exchangerateservice/ExchangeRateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.exchangerateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeRateServiceApplication.class, args);
+    }
+
+}
