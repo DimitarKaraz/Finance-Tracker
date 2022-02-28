f70c17be824785a8c06a76a12ba007bfe4265e44
Dimitar Karazapryanov <dkarazapr@gmail.com>
2022-02-28 16:25:47 +0200

    fixed endpoint.jpg, .pptx

--- FinanceTracker/src/main/java/com/example/financetracker/model/dto/userDTOs/UserRegisterFormDTO.java
+++ FinanceTracker/src/main/java/com/example/financetracker/model/dto/userDTOs/UserRegisterFormDTO.java
@@ -1,5 +1,6 @@
 package com.example.financetracker.model.dto.userDTOs;
 
+import com.example.financetracker.passwordValidators.ValidPassword;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,7 +27,7 @@ public class UserRegisterFormDTO implements Serializable {
 
     // For easier demonstration, the @ValidPassword is commented out.
 
-//    @ValidPassword
+    @ValidPassword
     @NotBlank
     private String password;
 
